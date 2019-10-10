08e40134792542eb16f7db2dbd33e58b8fa096f6
Gonchar-AV <avgonchar@sberbank.ru>
2019-10-10 21:28:24 +0300

    commit4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello world");
-        System.out.println("Hello world3332");
+        System.out.println("Hello world3432");
     }
     int var1 = 1;
     int var2 = 2;
